56c57dc8c8564af280d79fbebd1b6ab6c87b0548
Peter Larsson <peter@kodifiera.se>
2020-11-10 14:08:13 +0100

    Added shell env

--- wind/build.sh
+++ wind/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 NAME=$(grep name ${DIR}/package.json | sed 's/.*"name": "\(.*\)".*/\1/')
 VERSION=$(grep version ${DIR}/package.json | sed 's/.*"version": "\(.*\)".*/\1/')
