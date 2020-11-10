944692415ee727d7fe4295b57fbcba15b8e2f92b
Peter Larsson <peter@kodifiera.se>
2020-11-10 13:29:30 +0100

    Added docker build script

---
+++ wind/build.sh
@@ -0,0 +1,4 @@
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+NAME=$(grep name ${DIR}/package.json | sed 's/.*"name": "\(.*\)".*/\1/')
+VERSION=$(grep version ${DIR}/package.json | sed 's/.*"version": "\(.*\)".*/\1/')
+docker build -t "${NAME}:${VERSION}" $DIR
