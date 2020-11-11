7f5326e96c453c4e7d2a3b63975646458cf19eda
magda <magdaa.a@hotmail.com>
2020-11-11 14:46:16 +0100

    temperature

---
+++ temperature/src/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var axios = require('axios');
+const app = express();
+const LRU = require('lru-cache');
+const PORT = process.env.PORT ||3000;
+const cache = new LRU({'maxAge':600000});
+
+app.listen(PORT, () => console.log (`Server started on port ${PORT}`));
+
+app.get("/", async(req, res) => {
+    const temp = await getTemp();
+    res.send("" + temp)
+})
+
+
+const getTemp = async () => {
+    let url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/22.14/lat/65.58/data.json';
+    var data = cache.get(url);
+    if (!data) {
+        const res = await axios.get(url)
+        if(res.status == 200) {
+            data = res.data.timeSeries[0].parameters[1].values[0];
+            cache.set(url, data);
+        }
+        else {
+            res.status(500);
+            return("Bad request: " + res.status);
+        }
+    }        
+    return(data);
+}
+
