202c4c7820a84881f49f09f5516bd694ebfb2ae6
magda <magdaa.a@hotmail.com>
2020-11-15 20:23:37 +0100

    get temperature works

--- el-consumption/src/index.js
+++ el-consumption/src/index.js
@@ -1,5 +1,31 @@
 const express = require('express');
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT ||3001;
 var axios = require('axios');
 const app = express();
 
+
+app.listen(PORT, () => console.log (`Server started on port ${PORT}`));
+
+app.get("/", async(req, res) => {
+    const temp = await getTemp();
+    res.send("" + temp.data)
+})
+
+const getTemp = async () => {
+    try {
+        const res = await axios.get('http://localhost:3000/');
+        if(res.status == 200) {
+            temp = res;
+            return temp;
+        }
+        else {
+            res.status(500);
+            return("Bad request: " + res.status);
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+    
+
+}
