46f241812f4c6c68899459fbad3ef4ded1ebbf2d
Peter Larsson <peter@kodifiera.se>
2020-11-16 12:07:50 +0100

    Created electricity price module

---
+++ el-price/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import price from "./price.js";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+	res.status(200);
+	res.send("" + price());
+});
+
+app.listen(port, () => {
+	console.info(`Example app listening at http://localhost:${port}`);
+});
