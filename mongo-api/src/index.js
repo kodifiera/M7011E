20dd98d1dfa88eb631caa441487aff7bd1bc19cd
magda <magdaa.a@hotmail.com>
2020-12-09 12:33:26 +0100

    cors handling

--- mongo-api/src/index.js
+++ mongo-api/src/index.js
@@ -25,6 +25,19 @@ const store = Store();
 app.use(bodyParser.json());
 app.use(expressLogger);
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization",
+	);
+	if (req.method === "OPTIONS") {
+		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+		return res.status(200).json({});
+	}
+	next();
+});
+
 /**
  * Use API sub-endpoints
  */
