41b0cad09071714e042968ed1e7ac98bef26f86d
magda <magdaa.a@hotmail.com>
2020-12-16 14:17:36 +0100

    display image

--- prosumer/src/app/components/image-upload/image-upload.component.ts
+++ prosumer/src/app/components/image-upload/image-upload.component.ts
@@ -9,6 +9,9 @@ import { ImageService } from './../../services/image.service';
 export class ImageUploadComponent implements OnInit {
 
   selectedFile: any;
+  imageToShow: any;
+  isImageLoading: any;
+
 
   constructor(private imageService: ImageService) { }
   
@@ -32,20 +35,44 @@ export class ImageUploadComponent implements OnInit {
   private onSuccess() {
     this.selectedFile.pending = false;
     this.selectedFile.status = 'ok';
+    this.selectedFile.exists = true;
+    this.getImageFromService();
   }
 
   private onError() {
     this.selectedFile.pending = false;
-    this.selectedFile.status = 'fail';
+    this.selectedFile.status = 'error';
     this.selectedFile.src = '';
   }
 
   ngOnInit(): void {
+    this.getImageFromService()
   }
-
+  createImageFromBlob(image: Blob) {
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+       this.imageToShow = reader.result;
+    }, false);
+ 
+    if (image) {
+       reader.readAsDataURL(image);
+    }
+ }
+ getImageFromService() {
+  this.isImageLoading = true;
+  this.imageService.getImage().subscribe(data => {
+    this.createImageFromBlob(data);
+    this.isImageLoading = false;
+  }, error => {
+    this.isImageLoading = false;
+    if(error.status===500){
+      console.log("nja");
+    }
+  });
+}
 }
 class ImageSnippet {
   constructor(public src: string, public file: File) {}
   pending: boolean = false;
-  status: string = "";
+  status: string = '';
 }
